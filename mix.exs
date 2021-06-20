4cc6440dc697a28fe999ccd2e7a981626972d0f5
Susumu Yamazaki <zacky1972@gmail.com>
2021-06-20 18:24:19 +0900

    install Nx

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule NxMicroBench.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nx, "~> 0.1.0-dev", github: "elixir-nx/nx", branch: "main", sparse: "nx"}
     ]
   end
 end

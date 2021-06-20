669a5e18f41010ea96247981b271ac5143508404
Susumu Yamazaki <zacky1972@gmail.com>
2021-06-20 18:51:32 +0900

    remove test function

--- lib/nx_micro_bench.ex
+++ lib/nx_micro_bench.ex
@@ -2,17 +2,4 @@ defmodule NxMicroBench do
   @moduledoc """
   Documentation for `NxMicroBench`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NxMicroBench.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/nx_micro_bench_test.exs
+++ test/nx_micro_bench_test.exs
@@ -1,8 +1,4 @@
 defmodule NxMicroBenchTest do
   use ExUnit.Case
   doctest NxMicroBench
-
-  test "greets the world" do
-    assert NxMicroBench.hello() == :world
-  end
 end
